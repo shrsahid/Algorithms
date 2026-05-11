b2d81f8ed26bcc71f73a128b3d63dd5c0fcee450
Sahid Hasan <shrsahid4@gmail.com>
2026-05-11 07:21:41 -0700

    Adding Binary Search Algo

--- Searching Algorithm/Code.cpp
+++ Searching Algorithm/Code.cpp
@@ -1,37 +1,51 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int lin_sear(int arr[], int n, int target) {
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == target) {
-            return i; // return index
+int bin_sear(int arr[ ],int n,int terget){
+    int left = 0;
+    int right =n - 1;
+    
+    while(left<=right){
+        int mid = (left + right)/2;
+
+        if(arr[mid]==terget){
+           return mid;
+        }
+        else if(arr[mid]<terget){
+            left= mid + 1;
+        }
+        else{
+            right= mid-1;
         }
     }
     return -1;
+
 }
 
-int main() {
-    int n, target;
+int main(){
+    int n, terget;
 
-    cout << "Enter your array size: ";
-    cin >> n;
+    cout<<"Enter your arr size: ";
+    cin>>n;
+
 
     int arr[n];
-
-    cout << "Enter " << n << " elements:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    cout<<"Enter a sorted Array: \n";
+    for(int i=0;i<n;i++){
+        cin>> arr[i];
     }
 
-    cout << "Enter the target element: ";
-    cin >> target;
+    cout<<"Enter your terget value: ";
+    cin>>terget;
 
-    int result = lin_sear(arr, n, target);
+    int result = bin_sear(arr,n,terget);
 
-    if (result != -1)
-        cout << "Element found at index no: " << result;
+    if(result != -1)
+        cout<<terget<<" is found at index: " << result;
+
     else
-        cout << "Element not found";
+        cout<<"Element is not found.";
+        return 0;
 
-    return 0;
+
 }
