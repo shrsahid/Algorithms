9133143db3c728d706bb051b4c1d794048e6047b
Sahid Hasan <shrsahid4@gmail.com>
2026-05-19 08:23:46 -0700

    Modified Bucket Sort Algo

--- Sorting Algorithms/bucket_sort.py
+++ Sorting Algorithms/bucket_sort.py
@@ -1,30 +1,22 @@
-# Bucket Sort in Python
-
 def bucket_sort(arr):
 
-    # Find maximum value
     max_value = max(arr)
 
-    # Create empty buckets
     bucket_count = len(arr)
     buckets = []
 
     for i in range(bucket_count):
         buckets.append([])
 
-    # Put array elements into buckets
     for num in arr:
 
-        # Calculate bucket index
         index = int((num / (max_value + 1)) * bucket_count)
 
         buckets[index].append(num)
 
-    # Sort each bucket
     for bucket in buckets:
         bucket.sort()
 
-    # Merge buckets into original array
     k = 0
 
     for bucket in buckets:
@@ -32,8 +24,6 @@ def bucket_sort(arr):
             arr[k] = num
             k += 1
 
-
-# Main Program
 numbers = []
 
 n = int(input("Enter number of elements: "))
